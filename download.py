3c961cd83aa025b63d10cebfb8f8c22d7a477605
CyanideCN <dpy274555447@gmail.com>
2023-07-10 20:23:36 +0800

    fix

--- download.py
+++ download.py
@@ -1,12 +1,11 @@
 import subprocess
 
 eccodes_version = '2.30.2'
-dir_name = f'{eccodes_version}-Source'
-url = f'https://confluence.ecmwf.int/download/attachments/45757960/{eccodes_version}-Source.tar.gz?api=v2'
+dir_name = f'eccodes-{eccodes_version}-Source'
+url = f'https://confluence.ecmwf.int/download/attachments/45757960/eccodes-{eccodes_version}-Source.tar.gz?api=v2'
 
 def call_command(cmd):
     return subprocess.run(cmd, shell=True, stdout=subprocess.PIPE)
 
-print(f'Downloading {url}')
 call_command(['curl', '-o', 'sources.tar.gz', url])
-call_command('7z x -tgzip -so sources.tar.gz | 7z x -si -ttar', check=True)
+call_command('7z x -tgzip -so sources.tar.gz | 7z x -si -ttar')
