39508ccf4862f7485008c1cd68c5153d802af51c
CyanideCN <dpy274555447@gmail.com>
2023-07-10 20:05:46 +0800

    Initial commit

---
+++ download.py
@@ -0,0 +1,13 @@
+import os
+import subprocess
+
+cwd = os.getcwd()
+eccodes_version = '2.30.2'
+dir_name = f'{eccodes_version}-Source'
+url = f'https://confluence.ecmwf.int/download/attachments/45757960/{eccodes_version}-Source.tar.gz?api=v2'
+
+def call_command(cmd):
+    return subprocess.run(cmd, shell=True, stdout=subprocess.PIPE)
+
+call_command(['curl', '-o', 'sources.tar.gz', url])
+call_command('7z x -tgzip -so sources.tar.gz | 7z x -si -ttar')
