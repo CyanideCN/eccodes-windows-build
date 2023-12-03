81fbba168daeec58277867d2bbaece738c16b54b
CyanideCN <dpy274555447@gmail.com>
2023-12-02 23:40:26 -0800

    Fix missing netcdf dll

--- post_process.py
+++ post_process.py
@@ -4,7 +4,8 @@ import os
 ori_dir = 'C:/Miniconda/envs/build/Library/bin'
 dst_dir = 'C:/eccodes/bin'
 
-for f in ['openjp2.dll', 'libpng16.dll', 'zlib.dll']:
+for f in ['openjp2.dll', 'libpng16.dll', 'zlib.dll', 'netcdf.dll',
+          'mfhdf.dll', 'hdf5_hl.dll', 'hdf5.dll', 'zip.dll']:
     print(f'Moving {os.path.join(ori_dir, f)}')
     shutil.copy2(os.path.join(ori_dir, f), dst_dir)
 
